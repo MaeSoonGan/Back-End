69e02129e02493f5fee48650db3dc7945a8739f6
sy <sykim0659@naver.com>
2026-05-19 17:51:55 +0900

    init: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/mock/maesoongan/MaeSoonGanApplication.java
@@ -0,0 +1,13 @@
+package com.mock.maesoongan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeSoonGanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaeSoonGanApplication.class, args);
+    }
+
+}
